d86894bef67c7a65c39c708937bd2d08d8d48dda
ArthuryanLoheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-09-25 13:42:17 +0200

    ✨ GIMP-38 Replace console output with SFML window rendering in main function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 # include <iostream>
 # include <string>
+# include <SFML/Graphics.hpp>
 
 int main() {
-    std::string str = "Hello, World!";
-    std::cout << str << std::endl;
-    return 0;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear(sf::Color::Red);
+        window.display();
+    }
 }
