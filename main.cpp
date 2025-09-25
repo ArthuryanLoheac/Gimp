865c48b9edaf4339db62f05bd7293f7e895139d3
ArthuryanLoheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-09-25 14:45:02 +0200

    GIMP-40 ✨ Update window settings and color in main function for improved rendering

--- main.cpp
+++ main.cpp
@@ -2,15 +2,22 @@
 # include <string>
 # include <SFML/Graphics.hpp>
 
+void initializeWindow(sf::RenderWindow& window) {
+    window.setFramerateLimit(60);
+    window.setVerticalSyncEnabled(true);
+}
+
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "My Gimp");
+    initializeWindow(window);
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        window.clear(sf::Color::Red);
+        window.clear(sf::Color::Black);
         window.display();
     }
 }
